b96c5410fe619d88b7db773d2c0626d07dabf472
Leo Mancini <mancini@leo.gd>
2019-01-28 22:26:19 -0800

    Updating Twilio chat

--- chat.php
+++ chat.php
@@ -1,12 +1,8 @@
 <?php
 	require_once "vendor/autoload.php";
 	use Twilio\TwiML\MessagingResponse;
-
-	// Set the content-type to XML to send back TwiML from the PHP Helper Library
 	header("content-type: text/xml");
 
-	$response = new MessagingResponse();
-	
 	$incoming = strtolower($_REQUEST['Body']);
 	
 	if($incoming == "i have jury duty") {
@@ -14,9 +10,12 @@
 	} else {
 		$response_body = "Sorry, I don't understand";
 	}
+
+	$response = new MessagingResponse();
 	
 	$response->message(
 		$response_body
 	);
 
 	echo $response;
+?>
